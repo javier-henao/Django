87ff0675fae85dec47ef53c5014841516a630315
Javier Henao <javher@MacBook-Air-de-Javier.local>
2025-11-24 01:56:12 -0500

    creacion de ruta dinamica

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("monday",views.monday),
-    path("tuesday",views.tuesday),
-    path("wednesday",views.wednesday)
+    path("<day>", views.days_week), # /quote/monday...
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hola mundo desde Django!")
-
-def monday(request):
-    return HttpResponse("Hola lunes!")
-
-def tuesday(request):
-    return HttpResponse("Hola martes!")
-
-def wednesday(request):
-    return HttpResponse("Hola miercoles!")
+def days_week(request, day):
+    quote_text=None
+    if day=='monday':
+        quote_text='Pienso, luego existo. '
+    elif day=='tuesday':
+        quote_text='La vida es un sueño. '
+    else:
+        return HttpResponseNotFound('Sin mensaje. ')
+    return HttpResponse(quote_text)
