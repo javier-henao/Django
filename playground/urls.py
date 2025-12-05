e74fb95be78ba13f25825df560fa06f0e50d2446
Javier Henao <javher@MacBook-Air-de-Javier.local>
2025-12-05 18:11:43 -0500

    uso de fragmentos de template (include tag)

--- playground/urls.py
+++ playground/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('phrases/', include("quotes.urls")),
+    path('messages/', include("quotes.urls")),
     path('landings/', include("landing.urls")),
     
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,43 +1,37 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
 # Create your views here.
 
-days_of_week={
-    'monday':'Pienso, luego existo. ',
-    'tuesday':'La vida es un sueño. ',
-    'wednesday':'El tiempo es oro. ',
-    'thursday':'Se el cambio que quieres ver. ',
-    'friday':'Solo se que nada se. ',
-    'saturday':'El conocimiento es poder. ',
+days_of_week = {
+    'monday': 'Pienso, luego existo. ',
+    'tuesday': 'La vida es un sueño. ',
+    'wednesday': 'El tiempo es oro. ',
+    'thursday': 'Se el cambio que quieres ver. ',
+    'friday': 'Solo se que nada se. ',
+    'saturday': 'El conocimiento es poder. ',
     'sunday': 'La vida es bella. '
 }
 
+
 def index(request):
-    list_items = ""
-    days = list(days_of_week.keys()) # [monday, tuesday, ...]
-    
-    for day in days:
-        day_path = reverse("day-quote", args=[day])
-        list_items += f"<li><a href='{day_path}'>{day}</a></li>"
-        
-    response_html = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_html)
+    days = list(days_of_week.keys())  # [monday, tuesday, ...]
+    return render(request, "quotes/index.html", {"days": days})
+
 
 def days_week_with_number(request, day):
-    days=list(days_of_week.keys())
+    days = list(days_of_week.keys())
     if day > len(days):
         return HttpResponseNotFound("El dia no existe o es invalido")
-    redirect_day=days[day-1]
-    redirect_path=reverse("day-quote", args=[redirect_day])
+    redirect_day = days[day-1]
+    redirect_path = reverse("day-quote", args=[redirect_day])
     return HttpResponseRedirect(redirect_path)
-    
+
+
 def days_week(request, day):
     try:
-        quote_text=days_of_week[day] 
+        quote_text = days_of_week[day]
         return HttpResponse(quote_text)
-    except Exception: # o KeyError
-        return HttpResponseNotFound(f'{day}: No es valido ')
-
-
+    except Exception:  # o KeyError
+        return HttpResponseNotFound(f'{day}: No es valido <a href="#" onclick="history.back();">Regresar atrás</a>')
